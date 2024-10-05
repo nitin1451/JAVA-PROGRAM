d17a07e287a08f873d9a4a8887379355d9693bbd
nitin1451 <94160625+nitin1451@users.noreply.github.com>
2024-10-05 20:43:46 +0530

    Create income_tax_calculator.java
    This is kind of prototype of income tax calculator .

---
+++ income_tax_calculator.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class IncomeTaxCalculator
+{
+    public static void main (String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter your yearly income : ");
+        int income= sc.nextInt();
+        int tax;
+        
+        if (income < 500000)
+        {
+            tax=0;
+        }
+        else if (income>=500000&&income<1000000)
+        {
+            tax= (int)(income*0.2);
+        }
+        else
+        {
+            tax=(int)(income*0.3);
+        }
+        System.out.println("Tax is " + tax);
+
+
+    }
+    
+}
